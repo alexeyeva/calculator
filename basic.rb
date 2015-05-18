6a357dadd7d98f6b00468d4a1be980ac0748d75f
Kate Alexeyeva <kateryna.alexeyeva@gmail.com>
2015-05-18 14:50:55 +0300

    fix

--- basic.rb
+++ basic.rb
@@ -9,17 +9,3 @@ post '/credit_calculations' do
   erb :credit_calculations, locals: {credit_sum: params[:credit_sum].to_f, number_of_periods: params[:number_of_periods].to_i, interest_rate: params[:interest_rate].to_f}
 end
 
-class Calculator
-  def calculate(credit_sum, number_of_periods, interest_rate)
-    basic_debt = credit_sum / number_of_periods
-    credit_balance = credit_sum
-    annuity_credit_balance = credit_sum
-    credit_overpayment = 0
-    annuity_overpayment = 0
-    annuity_liquidated_debt = 0
-    month_interest_rate = (interest_rate / 100) / 12
-     d1 = ((1 + month_interest_rate)**number_of_periods) - 1
-     s1 = month_interest_rate + (month_interest_rate / d1)
-     annuity_due = credit_sum * s1
-  end
-end
